5881dec682ce979fd1b8d75765e4178608c55a42
Emanuel Carvalho <114405488+emanuelmyx@users.noreply.github.com>
2025-03-11 16:04:39 -0300

    Add files via upload

---
+++ questao11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+
+float gan1, gan2, gan3;
+
+gan1 = 780.000 * 0.46;
+gan2 = 780.000 * 0.32;
+gan3 = 780.000 * 0.14;
+
+printf("R$ %.3f \n", gan1);
+printf("R$ %.3f \n", gan2);
+printf("R$ %.3f \n", gan3);
+
+return 0;    
+}
