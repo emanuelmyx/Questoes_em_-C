c14ffbc7ba9c7d0fdc92a9ac84087169be5070b9
Emanuel Carvalho <114405488+emanuelmyx@users.noreply.github.com>
2025-03-11 15:04:04 -0300

    Add files via upload

---
+++ questao8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void){
+
+int num1, num2, num3;
+
+printf("Digite o primeiro numero: ");
+scanf("%i", &num1);
+
+
+printf("Digite o segundo numero: ");
+scanf("%i", &num2);
+
+printf("Digite o  terceiro numero: ");
+scanf("%i", &num3);
+
+
+printf("Soma: %i\n", (num1 + num2 + num3));
+
+
+return 0;    
+}
